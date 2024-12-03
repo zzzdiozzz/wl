f6bbdccc9b8401477a2d65fa601ff77e963398e1
Dmitry Zorin <zorin.dmitri@gmail.com>
2024-12-03 20:09:21 +0200

    first commit from dell

---
+++ routes/root.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = async function (fastify, opts) {
+  fastify.get('/', async function (request, reply) {
+//    return { root: true }
+return ('fucking crazy')
+  })
+}
